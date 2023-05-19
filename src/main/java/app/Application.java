c15fd2e1fa2bb2634e1fedff84b3cba2fe23c6c5
Nestor162 <nestorcicardini@gmail.com>
2023-05-19 13:45:33 +0200

    Istanziati utenti

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.time.LocalDate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
@@ -7,8 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dao.PublicationDAO;
+import dao.UserDAO;
 import entities.Book;
 import entities.Magazine;
+import entities.User;
 import utils.JpaUtil;
 
 public class Application {
@@ -20,6 +24,7 @@ public class Application {
 		EntityManager em = emf.createEntityManager();
 
 		PublicationDAO pd = new PublicationDAO(em);
+		UserDAO ud = new UserDAO(em);
 
 		Book book1 = new Book("9780553573398", "Assassin's Apprentice", 1995,
 				464, "Robin Hobb", "Fantasy");
@@ -39,12 +44,25 @@ public class Application {
 		Magazine magazine3 = new Magazine("9789999999999", "New Scientist",
 				2021, 52, Magazine.publicationFrequency.MONTHLY);
 
-		pd.save(book1);
-		pd.save(book2);
-		pd.save(book3);
-		pd.save(magazine1);
-		pd.save(magazine2);
-		pd.save(magazine3);
+		User user1 = new User("Marco", "Rossi", "Via Roma 123",
+				"marco.rossi@example.com", "+39 123456789", LocalDate.now());
+
+		User user2 = new User("Giulia", "Bianchi", "Via Dante 456",
+				"giulia.bianchi@example.com", "+39 987654321", LocalDate.now());
+
+		User user3 = new User("Luca", "Ferrari", "Corso Vittorio Emanuele 789",
+				"luca.ferrari@example.com", "+39 456789123", LocalDate.now());
+
+//		pd.save(book1);
+//		pd.save(book2);
+//		pd.save(book3);
+//		pd.save(magazine1);
+//		pd.save(magazine2);
+//		pd.save(magazine3);
+
+//		ud.save(user1);
+//		ud.save(user2);
+//		ud.save(user3);
 
 		em.close();
 		emf.close();
