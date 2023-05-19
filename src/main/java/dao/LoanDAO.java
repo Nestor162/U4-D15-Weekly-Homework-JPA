9a221d3c9a499f0c3834730c862d713075f0218b
Nestor162 <nestorcicardini@gmail.com>
2023-05-19 13:48:28 +0200

    Creata classe LoanDao

---
+++ src/main/java/dao/LoanDAO.java
@@ -0,0 +1,52 @@
+package dao;
+
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import entities.Loan;
+import entities.User;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoanDAO {
+	private final EntityManager em;
+
+	public LoanDAO(EntityManager em) {
+		this.em = em;
+	}
+
+	public void save(User l) {
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.persist(l);
+		transaction.commit();
+		log.info("Presitto aggiunto!" + System.lineSeparator());
+	}
+
+	public void delete(String id) {
+		Loan found = em.find(Loan.class, UUID.fromString(id));
+		if (found != null) {
+			EntityTransaction transaction = em.getTransaction();
+			transaction.begin();
+			em.remove(found);
+			transaction.commit();
+			log.info("Prestito con id: " + id + " eliminato dal database!"
+					+ System.lineSeparator());
+		}
+	}
+
+	public void getById(String id) {
+		Loan found = em.find(Loan.class, UUID.fromString(id));
+		log.info("Trovato prestito con id: " + id + System.lineSeparator()
+				+ found.toString());
+	}
+
+	public void refresh(String id) {
+		Loan found = em.find(Loan.class, UUID.fromString(id));
+
+		em.refresh(found);
+		log.info("Ripristinato Evento ai valori: " + found);
+	}
+}
