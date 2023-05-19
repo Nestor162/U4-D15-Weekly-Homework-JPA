ab3a79bc2e6c2f30d22c4f1406f018d50b0f6ab8
Nestor162 <nestorcicardini@gmail.com>
2023-05-19 11:56:48 +0200

    Creata classe User

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,51 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private String name;
+	private String surname;
+	private String addres;
+	@Column(length = 319)
+	private String email;
+	private String phoneNumber;
+	private LocalDate registrationDate;
+
+	public User(String name, String surname, String addres, String email,
+			String phoneNumber, LocalDate registrationDate) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.addres = addres;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.registrationDate = registrationDate;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", surname=" + surname
+				+ ", addres=" + addres + ", email=" + email + ", phoneNumber="
+				+ phoneNumber + ", registrationDate=" + registrationDate + "]";
+	}
+
+}
