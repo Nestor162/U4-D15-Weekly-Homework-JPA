9913b44dee2b3010a1d510284d287278b010a48c
Nestor162 <nestorcicardini@gmail.com>
2023-05-19 12:26:23 +0200

    Creata classe Loan

---
+++ src/main/java/entities/Loan.java
@@ -0,0 +1,53 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "loans")
+public class Loan {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private LocalDate loanDate;
+	private LocalDate expectedReturnDate;
+	private LocalDate returnedDate;
+
+	@ManyToOne
+	@JoinColumn(name = "publication_id")
+	private Publication publication;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User users;
+
+	@Override
+	public String toString() {
+		return "Loan [id=" + id + ", loanDate=" + loanDate
+				+ ", expectedReturnDate=" + expectedReturnDate
+				+ ", returnedDate=" + returnedDate + "]";
+	}
+
+	public Loan(LocalDate loanDate, LocalDate expectedReturnDate,
+			LocalDate returnedDate) {
+		super();
+		this.loanDate = loanDate;
+		this.expectedReturnDate = expectedReturnDate;
+		this.returnedDate = returnedDate;
+	}
+
+}
